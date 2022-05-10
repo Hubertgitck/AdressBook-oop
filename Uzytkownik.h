26e37f7675a7b587234d87349e848226e395de89
Hubert Kozieł <hubert.koziel.programista@gmail.com>
2022-05-10 19:39:54 +0200

    Added user registration

--- Uzytkownik.h
+++ Uzytkownik.h
@@ -4,5 +4,16 @@ using namespace std;
 
 class Uzytkownik{
     int id;
-    string login, haslo;
+    string login;
+    string haslo;
+
+public:
+    void ustawId(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string noweHaslo);
+
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();
+
 };
